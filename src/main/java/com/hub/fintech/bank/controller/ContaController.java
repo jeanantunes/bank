7f1c80567d565fa95a858471d236f0f113add8b4
Jean Antunes <jean.antunes@gmail.com>
2018-04-11 16:32:53 -0300

    Update Controller Conta Pessoa

--- src/main/java/com/hub/fintech/bank/controller/ContaController.java
+++ src/main/java/com/hub/fintech/bank/controller/ContaController.java
@@ -1,10 +1,13 @@
 package com.hub.fintech.bank.controller;
 
 import com.hub.fintech.bank.model.entity.Conta;
+import com.hub.fintech.bank.model.entity.Pessoa;
 import com.hub.fintech.bank.service.ContaService;
+import com.hub.fintech.bank.service.PessoaService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -15,59 +18,92 @@ import java.util.List;
  */
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/conta")
 public class ContaController {
 
     @Autowired
     private ContaService contaService;
 
+    @Autowired
+    private PessoaService pessoaService;
+
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
     //List
-    @GetMapping("/conta/list")
+    @GetMapping("/list")
     public List<Conta> getAllContas() {
-        return contaService.findAll();
+        List<Conta> c = contaService.findAll();
+        if (c.isEmpty()){
+            logger.error("Lista de Contas vazia: "+c);
+        }
+        return c;
     }
 
     // Save
-    @PutMapping(value = "/conta/save/{conta}", produces = "application/json")
-    public @ResponseBody
-    Conta saveConta(@Valid @RequestBody Conta conta) {
-        contaService.save(conta);
-        return conta;
+    public @RequestMapping(method = RequestMethod.PUT, value = "/save/{conta}", produces = "application/text")
+    String saveConta(@Valid @RequestBody Conta conta,
+                    @RequestParam(name = "pessoaId") Long pessoaId, Model model) {
+        model.addAttribute("pessoaId", pessoaId);
+        Pessoa p = pessoaService.findById(pessoaId);
+        if (p == null){
+            logger.error("PESSOA_ID: "+pessoaId+". Não Localizado. Obrigatório associar Pessoa a Conta.");
+            return "PESSOA_ID: "+pessoaId+". Não Localizado. Obrigatório associar Pessoa a Conta.";
+        }else {
+        conta.setPessoaId(pessoaId);
+        if (p.getCpf() != null || p.getCnpj() != null) {
+            p.setPessoaTipo(conta.getPessoaTipo());
+            p.setCpf(conta.getCpf());
+            p.setCnpj(conta.getCnpj());
+            p.setContaId(pessoaId);
+            pessoaService.save(p);
+            conta.setContaId(pessoaId);
+            contaService.save(conta);
+            logger.info("PESSOA_ID: "+pessoaId+" CONTA_ID: "+conta.getId()+". Conta criada com Sucesso.");
+            return "Conta criada com Sucesso.";
+            }else {
+            logger.error("Pessoa CPF ou CNPJ obrigatório.");
+            return "Pessoa CPF ou CNPJ Obrigatório. Atualize antes de associar a Conta.";
+        }
+        }
     }
 
     // Find
-    @PostMapping(value = "/conta/find/{id}", produces = "application/json")
+    @PostMapping(value = "/find/{id}", produces = "application/json")
     public @ResponseBody
     Conta getContaById(@PathVariable(value = "id") Long id) {
-        return contaService.findById(id);
+        Conta c = contaService.findById(id);
+        if (c == null){
+            logger.error("ID: "+id+" não Localizado.");
+        }
+        return c;
     }
 
     // Update
-    @PutMapping(value = "/conta/update/{id}", produces = "application/json")
+    @PutMapping(value = "/update/{id}", produces = "application/text")
     public @ResponseBody
-    Conta updateConta(@PathVariable(value = "id") Long id,
+    String updateConta(@PathVariable(value = "id") Long id,
                       @Valid @RequestBody Conta contaDetails) {
-
-        Conta conta = contaService.findById(id);
-        if (conta != null) {
-            contaService.save(contaDetails);
+        Conta c = contaService.findById(id);
+        if (c == null) {
+            logger.error("ID: "+id+". Não Localizado.");
+            return "ID: "+id+". Não Localizado.";
         } else {
-            System.out.println("Conta não encontrada");
+            contaService.save(contaDetails);
+            logger.info("Conta atualizada com Sucesso.");
         }
-        return contaDetails;
+        return "Atualizado com Sucesso.";
     }
 
     // Delete
-    @DeleteMapping("/conta/delete/{id}")
+    @DeleteMapping("/delete/{id}")
     public String deleteConta(@PathVariable(value = "id") Long id) {
-        Conta conta = contaService.findById(id);
-        if (conta != null) {
-            contaService.delete(id);
-            return "DELETE realizado com Sucesso.";
+        Conta c = contaService.findById(id);
+        if (c == null) {
+            logger.error("ID: "+id+". Não Localizado.");
+            return "Conta não localizada.";
         } else {
-            return "Conta não localizada";
+            contaService.delete(c);
+            return "Excluido com Sucesso.";
         }
     }
 }
--- src/main/java/com/hub/fintech/bank/controller/PessoaController.java
+++ src/main/java/com/hub/fintech/bank/controller/PessoaController.java
@@ -1,7 +1,11 @@
 package com.hub.fintech.bank.controller;
 
 import com.hub.fintech.bank.model.entity.Pessoa;
+import com.hub.fintech.bank.model.entity.Pf;
+import com.hub.fintech.bank.model.entity.Pj;
 import com.hub.fintech.bank.service.PessoaService;
+import com.hub.fintech.bank.service.PfService;
+import com.hub.fintech.bank.service.PjService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,59 +19,79 @@ import java.util.List;
  */
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/pessoa")
 public class PessoaController {
 
     @Autowired
     private PessoaService pessoaService;
 
+    @Autowired
+    private PfService pfService;
+
+    @Autowired
+    private PjService pjService;
+
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
     //List
-    @GetMapping("/pessoa/list")
+    @GetMapping("/list")
     public List<Pessoa> getAllPessoas() {
-        return pessoaService.findAll();
+        List<Pessoa> p = pessoaService.findAll();
+        if (p.isEmpty()){
+            logger.error("Lista de Pessoas vazia: "+p);
+        }
+        return p;
     }
 
     // Save
-    @PutMapping(value = "/pessoa/save/{pessoa}", produces = "application/text")
+    @PutMapping(value = "/save/{pessoa}", produces = "application/text")
     public @ResponseBody
     String savePessoa(@Valid @RequestBody Pessoa pessoa) {
         pessoaService.save(pessoa);
+        if (pessoa.getContaId() == null){
+            logger.warn("PESSOA_ID: "+pessoa.getId()+" não possui Conta associada."+" CONTA_ID: "+pessoa.getContaId());
+            return "PESSOA_ID: "+pessoa.getId()+". Não possui Conta associada. Pessoa Salva com Sucesso.";
+        }
         return "Pessoa Salva com Sucesso.";
     }
 
     // Find
-    @PostMapping(value = "/pessoa/find/{id}", produces = "application/json")
+    @PostMapping(value = "/find/{id}", produces = "application/json")
     public @ResponseBody
     Pessoa getPessoaById(@PathVariable(value = "id") Long id) {
-        return pessoaService.findById(id);
+        Pessoa p = pessoaService.findById(id);
+        if (p == null){
+            logger.error("ID: "+id+" não Localizado.");
+        }
+        return p;
     }
 
     // Update
-    @PutMapping(value = "/pessoa/update/{id}", produces = "application/json")
+    @PutMapping(value = "/update/{id}", produces = "application/text")
     public @ResponseBody
-    Pessoa updatePessoa(@PathVariable(value = "id") Long id,
+    String updatePessoa(@PathVariable(value = "id") Long id,
                         @Valid @RequestBody Pessoa pessoaDetails) {
-
-        Pessoa pessoa = pessoaService.findById(id);
-        if (pessoa != null) {
-            pessoaService.save(pessoaDetails);
+        Pessoa p = pessoaService.findById(id);
+        if (p == null) {
+            logger.error("ID: "+id+". Não Localizado.");
+            return "ID: "+id+". Não Localizado.";
         } else {
-            System.out.println("Pessoa não encontrada");
+            pessoaService.save(pessoaDetails);
+            logger.info("Pessoa atualizada com Sucesso.");
         }
-        return pessoaDetails;
+        return "Atualizado com Sucesso.";
     }
 
     // Delete
-    @DeleteMapping("/pessoa/delete/{id}")
+    @DeleteMapping("/delete/{id}")
     public String deletePessoa(@PathVariable(value = "id") Long id) {
-        Pessoa pessoa = pessoaService.findById(id);
-        if (pessoa != null) {
-            pessoaService.delete(id);
-            return "DELETE realizado com Sucesso.";
-        } else {
-            return "Pessoa não localizada";
+        Pessoa p = pessoaService.findById(id);
+        if (p == null) {
+            logger.error("ID: "+id+". Não Localizado.");
+            return "Pessoa não localizada.";
+        }else {
+        pessoaService.delete(p);
+        return "Excluido com Sucesso.";
         }
     }
 }
